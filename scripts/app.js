57d43bd81e2d00afa2423c7f5f2c05079458aaff
Habby Whyte <bahybaj@hotmail.com>
2016-03-23 05:40:00 -0400

    added code for hw

--- scripts/app.js
+++ scripts/app.js
@@ -5,10 +5,10 @@ var app = {};
 app.initialize = function(){
   console.log("Starting app...");
   app.shoppingCart.initialize();
-  app.shoppingCart.addItem("EloquentJavascript", 20.00, 2, "small", "book");
-  app.shoppingCart.addItem("Raspberry Pi", 35.50, 2, "small", "electronics");
-  app.shoppingCart.addItem("Trampoline", 339.50, 40, "large", "exercise");
-  app.shoppingCart.addItem("Tesla X1", 79900.00, 5100, "ludicrous", "vehicle")
+  app.shoppingCart.initialize("EloquentJavascript", 20.00, 2, "small", "book");
+  app.shoppingCart.initialize("Raspberry Pi", 35.50, 2, "small", "electronics");
+  app.shoppingCart.initialize("Trampoline", 339.50, 40, "large", "exercise");
+  app.shoppingCart.initialize("Tesla X1", 79900.00, 5100, "ludicrous", "vehicle")
 
   app.shoppingCart.render();
 }
